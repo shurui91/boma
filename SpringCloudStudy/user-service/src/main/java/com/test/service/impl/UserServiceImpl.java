a2cc0d6ab5a0dc3c35aec80d33ad552f08eb43fa
Aaron Liu <shurui91@gmail.com>
2024-03-20 15:47:08 -0700

    init commit

---
+++ SpringCloudStudy/user-service/src/main/java/com/test/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.test.service.impl;
+
+import com.test.entity.User;
+import com.test.mapper.UserMapper;
+import com.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User getUserById(int uid) {
+        return userMapper.getUserById(uid);
+    }
+}
